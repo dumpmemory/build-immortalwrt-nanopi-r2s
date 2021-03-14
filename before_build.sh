c4b821c9d341f76d50853ca35cfc8727d0cb4e47
AkinoKaede <autmaple@protonmail.com>
2021-03-14 23:10:31 +0800

    Remove luci-app-r2sflasher

--- before_build.sh
+++ before_build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 cd /home/runner/immortalwrt
 
-mkdir package/swc
-pushd "package/swc"
-git init
-git remote add origin https://github.com/songchenwen/nanopi-r2s.git
-git config core.sparsecheckout true
-echo "luci-app-r2sflasher" >> .git/info/sparse-checkout
-git pull --depth 1 origin master
-popd
-
 # Enter your commands here, e.g.
 # echo "Start build!"
 make defconfig
